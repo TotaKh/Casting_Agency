367421dffcf62d959c2cf3513545d1b873c0c84b
TotaKh <taghreed.khalawi@gmail.com>
2021-02-25 01:20:19 +0300

    add error handlers

--- app.py
+++ app.py
@@ -127,7 +127,48 @@ def create_app(test_config=None):
         abort(422)
 
 
+  #----------------------------------------------------------------------------#
+  # Error Handlers.
+  
+  @app.errorhandler(400)
+  def Bad_Request(error):
+    return jsonify({
+      'success': False,
+      'error': 400,
+      'message': "Bad Request"
+    }),400
 
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': "resource not found"
+    }),404
+
+  @app.errorhandler(405)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 405,
+      'message': "method not allowed"
+    }),405
+
+  @app.errorhandler(422)
+  def Unprocessable(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': "unprocessable"
+    }),422
+
+    @app.errorhandler(500)
+    def exception_handler(error):
+      return jsonify({
+        'success': False,
+        'error': 500,
+        'message': "internal server error"
+      }), 500
 
   return app
 
