a0a32066ef8e9898268beab3650cd26df20492da
TotaKh <taghreed.khalawi@gmail.com>
2021-02-24 22:21:47 +0300

    create models

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-
-from sqlalchemy import Column, String, create_engine
+import os
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 database_path = os.environ['DATABASE_URL']
@@ -12,30 +13,82 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.app = app
+  db.init_app(app)
+  migrate = Migrate(app, db)
+  db.create_all()
 
 
 '''
-Person
+Movies
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class Movies(db.Model):  
+  __tablename__ = 'movies'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release = Column(String)
+  #actors = Column(String)
+
+  def __init__(self, title, release ):
+    self.title = title
+    self.release = release
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release': self.release
+      }
+
+
+'''
+Actors
+Have name, age and gender
+'''
+class Actors(db.Model):
+  __tablename__ = 'actors'
 
   id = Column(Integer, primary_key=True)
   name = Column(String)
-  catchphrase = Column(String)
+  age = Column(String)
+  gender = Column(String)
 
-  def __init__(self, name, catchphrase=""):
+  def __init__(self, name, age,gender ):
     self.name = name
-    self.catchphrase = catchphrase
+    self.age = age
+    self.gender = gender
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'catchphrase': self.catchphrase}
+      'age': self.age,
+      'gender': self.gender
+      }
